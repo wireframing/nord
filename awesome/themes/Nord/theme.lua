c250ab56404e87018c6c6d7297385011ec7e7fe9
wireframing <87279975+wireframing@users.noreply.github.com>
2021-09-20 23:31:07 +0100

    Add files via upload

---
+++ awesome/themes/Nord/theme.lua
@@ -0,0 +1,77 @@
+-- Nord
+------------------
+
+local theme = {}
+
+theme.font          = 'Hack Nerd Font Regular 10'
+
+theme.bg_normal     = "#2e3440"
+theme.bg_focus      = "#d8dee9"
+theme.bg_urgent     = "#d8dee9"
+theme.bg_minimize   = "#d8dee9"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#2e3440"
+theme.fg_focus      = "#d8dee9"
+theme.fg_urgent     = "#5E9DAE"
+theme.fg_minimize   = "#327B8E"
+
+theme.useless_gap   = 7
+theme.border_width  = 0
+theme.border_normal = "#665c54"
+theme.border_focus  = "#dfbf8e"
+theme.border_marked = "#5E9DAE"
+
+-- Hotkeys popup
+theme.hotkeys_bg = '#2e3440'
+theme.hotkeys_fg = '#d8dee9'
+theme.hotkeys_opacity = '1'
+theme.hotkeys_border_width = 3
+theme.hotkeys_border_color = '#a5abb6'
+theme.hotkeys_modifiers_fg = '#a5abb6'
+theme.hotkeys_label_fg = '#a5abb6'
+theme.hotkeys_font = 'Hack Nerd Font Regular 10'
+theme.hotkeys_description_font = 'Hack Nerd Font Regular 10'
+theme.hotkeys_group_margin = 20
+
+-- Wallpaper
+theme.wallpaper = '~/.config/awesome/themes/Nord/wallpaper.png'
+
+return theme
+
+--#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+--#! Nord Material Dark
+--#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+--#*background: #1d2021
+--#! medium
+--#! *background: #2e3440
+--#! soft
+--#! *background: #32302f
+
+--#*foreground: #d4be98
+
+--#! Black + DarkGrey
+--#*color0:  #32302f
+--#*color8:  #32302f
+--#! DarkRed + Red
+--#*color1:  #ea6962
+--#*color9:  #ea6962
+--#! DarkGreen + Green
+--#*color2:  #d8dee9
+--#*color10: #d8dee9
+--#! DarkYellow + Yellow
+--#*color3:  #e78a4e
+--#*color11: #e78a4e
+--#! DarkBlue + Blue
+--#*color4:  #7daea3
+--#*color12: #7daea3
+--#! DarkMagenta + Magenta
+--#*color5:  #d3869b
+--#*color13: #d3869b
+--#! DarkCyan + Cyan
+--#*color6:  #89b482
+--#*color14: #89b482
+--#! LightGrey + White
+--#*color7:  #d4be98
+--#*color15: #d4be98--#3c404c
+
